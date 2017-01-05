ec8d6e60ad1b2832139c438a0c9589818775dd4a
Nicolás Silva <sovietfrance@gmail.com>
2017-01-05 12:21:01 -0300

    feat(app): Update displayImgs method

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -2,31 +2,41 @@ import {SHOW_ALL} from '../constants/TodoFilters';
 import {initialTodo} from '../todos/todos';
 
 class AppController {
-  constructor(datasetService) {
-    this.todos = [initialTodo];
-    this.filter = SHOW_ALL;
-    this.datasetService = datasetService
-    this.grabs = null
-    this.movie = null
-    this.displayGrabs();
-  }
-  displayGrabs(){
-    this.datasetService
-      .getMovies()
-      .then((grabs) => {
-        this.grabs = grabs.data
-      })
-  }
-  setMovie(grab){
-    console.log(this.grabs)
-    this.movie = grab
-  }
-  paginate(){
-    console.log("paginate...")
-  }
+    constructor(datasetService) {
+        this.todos = [initialTodo];
+        this.filter = SHOW_ALL;
+        this.datasetService = datasetService;
+        this.grabs = null;
+        this.movie = null;
+        this.imageList = [];
+        this.displayGrabs();
+    }
+    displayGrabs() {
+        this.datasetService
+          .getMovies()
+          .then((grabs) => {
+            this.grabs = grabs.data
+            this.displayImgs()
+        })
+    }
+    setMovie(grab) {
+        console.log(this.grabs)
+        this.movie = grab
+    }
+    displayImgs() {
+        this.grabs.map((grab) => {
+            this.datasetService
+              .getImagesByID(grab.id)
+              .then((list) => {
+                this.imageList[grab.id] = list.data
+            })
+        })
+    }
+    paginate() {
+        console.log("paginate...")
+    }
 }
-
 export const App = {
-  template: require('./App.html'),
-  controller: AppController
+    template: require('./App.html'),
+    controller: AppController
 };
